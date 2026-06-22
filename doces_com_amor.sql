f2c1295fd57295e99a8bd33da7c7ed93f8f49e18
Guilherme Santos Dias <guilherme.sdias@sptech.school>
2026-06-21 23:29:10 -0300

    Atualização banco de dados

--- doces_com_amor.sql
+++ doces_com_amor.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE doces_com_amor;
+
 USE doces_com_amor;
 
-CREATE TABLE admin (
+CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(30) NOT NULL,
+    nome VARCHAR(60) NOT NULL,
+    username VARCHAR(30) NOT NULL,
     senha VARCHAR(30) NOT NULL
 );
 
@@ -11,7 +13,7 @@ CREATE TABLE cliente (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    is_ativo BOOLEAN DEFAULT TRUE,
+    is_ativo BOOLEAN NOT NULL DEFAULT TRUE,
     telefone VARCHAR(11)
 );
 
@@ -25,59 +27,47 @@ CREATE TABLE endereco (
     FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
 
-CREATE TABLE entrega (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE entrega(
+	id INT AUTO_INCREMENT PRIMARY KEY,
     estado VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE pagamento (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE pagamento(
+	id INT AUTO_INCREMENT PRIMARY KEY,
     estado VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE pedido (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT,
     id_cliente INT NOT NULL,
     id_endereco INT NOT NULL,
-    produto VARCHAR(100) NOT NULL,
-    descricao VARCHAR(200),
+    descricao VARCHAR(100) NOT NULL,
+    detalhes VARCHAR(200),
     valor_total DECIMAL(10,2) NOT NULL,
-    status_entrega INT NOT NULL,
+	status_entrega INT NOT NULL,
     status_pagamento INT NOT NULL,
-    is_ativo BOOLEAN NOT NULL DEFAULT TRUE,
-    is_reagendado BOOLEAN NOT NULL DEFAULT FALSE,
+	is_ativo BOOLEAN NOT NULL DEFAULT TRUE,
+	is_reagendado BOOLEAN NOT NULL DEFAULT FALSE,
     data_pedido DATETIME NOT NULL,
     data_modificacao DATETIME,
     data_criacao DATETIME NOT NULL,
+    PRIMARY KEY (id, id_cliente),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id),
     FOREIGN KEY (id_endereco) REFERENCES endereco(id),
-    FOREIGN KEY (status_entrega) REFERENCES entrega(id),
+	FOREIGN KEY (status_entrega) REFERENCES entrega(id),
     FOREIGN KEY (status_pagamento) REFERENCES pagamento(id)
 );
 
-CREATE TABLE categoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE item_pedido (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    quantidade INT NOT NULL,
-    valor_unitario DECIMAL(7,2) NOT NULL,
-    observacao VARCHAR(150),
-    id_pedido INT NOT NULL,
-    id_categoria INT NOT NULL,
-    FOREIGN KEY (id_pedido) REFERENCES pedido(id),
-    FOREIGN KEY (id_categoria) REFERENCES categoria(id)
-);
-
 CREATE TABLE historico_pedido (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT,
     id_pedido INT NOT NULL,
-    status_entrega INT NOT NULL,
+    id_cliente INT NOT NULL,
+	status_entrega INT NOT NULL,
     status_pagamento INT NOT NULL,
     data_criacao DATETIME NOT NULL,
+	PRIMARY KEY (id, id_pedido, id_cliente),
     FOREIGN KEY (id_pedido) REFERENCES pedido(id),
-    FOREIGN KEY (status_entrega) REFERENCES entrega(id),
+	FOREIGN KEY (id_cliente) REFERENCES pedido(id_cliente),
+	FOREIGN KEY (status_entrega) REFERENCES entrega(id),
     FOREIGN KEY (status_pagamento) REFERENCES pagamento(id)
 );
